7b93cb302b1040fca29f7d6e59b8b9e9ac278365
Vinicius Da Silva Cruz <vinicius.da.silva.cruz.sanji@gmail.com>
2021-03-21 12:19:20 -0300

    Criação do projeto servidor

---
+++ servidor-aps/src/main/java/br/com/unip/servidoraps/ServidorApsApplication.java
@@ -0,0 +1,13 @@
+package br.com.unip.servidoraps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorApsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorApsApplication.class, args);
+	}
+
+}
---
+++ servidor-aps/src/test/java/br/com/unip/servidoraps/ServidorApsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unip.servidoraps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorApsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
